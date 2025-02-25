83895f525829f93f07b403a0eb0ccee2d1d577ea
Ivan Artamonov <artamonov.i@studio2i.ru>
2025-02-25 10:45:48 +0300

    init

---
+++ base/src/AppServiceProvider.php
@@ -0,0 +1,14 @@
+<?php namespace Chunker2i\Base\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+	public function register():void {
+		//
+	}
+
+	public function boot():void {
+		//
+	}
+}
